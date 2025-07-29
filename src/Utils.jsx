971804638b2cd18005f4b3d7429648573a108d8c
Sahid110044 <139684859+Sahid110044@users.noreply.github.com>
2025-07-29 17:36:21 +0530

    Update Utils.jsx

--- src/Utils.jsx
+++ src/Utils.jsx
@@ -4,4 +4,4 @@ import {toast} from 'react-toastify'
 export const notify = (message,type)=>{
        toast[type](message)
 }
-export const API_URL = 'http://localhost:3001'
+export const API_URL = 'https://task-backend-indol.vercel.app'
